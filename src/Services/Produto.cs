16d609772a3ba823af549ef320c6b5e63923a266
Daniel <daniel6991ferreira@gmail.com>
2020-10-31 23:25:48 -0300

    Atualizando

--- src/Services/Produto.cs
+++ src/Services/Produto.cs
@@ -1,11 +1,17 @@
 using Models;
-
+using System.Linq;
 namespace Services
 {
     public class Produto : ProdutoInitialize
     {
+        public ProdutoModel[] produto {get;}
         public Produto(): base()
-        {}
+        {produto = base.Produtos();}
+
+        public ProdutoModel GetProdutoByName(string name)
+        {
+            return produto.Where(x => x.Produto == name).First();
+        }
 
     }
 }
--- tests/ProdutoTest.cs
+++ tests/ProdutoTest.cs
@@ -10,10 +10,14 @@ namespace tests
         Produto prd;
         public ProdutoTest() =>
             this.prd = new Produto();
-        
+
         [Fact]
-        public void ProdutosNaoVazios() => 
-            prd.Produtos().
+        public void ProdutosNaoVazios() {
+            prd.produto.Should().HaveCountGreaterThan(0, "A lista não pode ser vazia");}
+        
+        [Theory]
+        [InlineData("Torta de Maça")]
+        public void GetProdutos(string name) 
+            {prd.GetProdutoByName(name).Produto.Should().Be(name, "os nomes precisam ser iguam");}
     }
 }
-            
